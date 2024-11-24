30e4dc7f8d6d8080e0765d6bba8e8bcccddf1696
japjotsaggu-wai <i-japjot@wadhwaniai.org>
2024-11-24 16:26:11 +0530

    Create setup_env.sh

---
+++ setup_env.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+pip install nltk sacremoses pandas regex mock transformers>=4.33.2 mosestokenizer
+pip install bitsandbytes scipy accelerate datasets
+pip install sentencepiece
+
+
+# Exit the script on errors
+set -e
+
+VENV_DIR="it2_idsp"
+
+echo "Setting up a Python virtual environment..."
+# Check if the virtual environment already exists
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv $VENV_DIR
+    echo "Virtual environment created at $VENV_DIR"
+else
+    echo "Virtual environment already exists at $VENV_DIR"
+fi
+
+# Activate the virtual environment
+source $VENV_DIR/bin/activate
+echo "Virtual environment activated."
+
+# Upgrade pip
+echo "Upgrading pip..."
+python3 -m pip install --upgrade pip
+
+echo "Cloning IndicTransToolkit repository..."
+if [ ! -d "IndicTransToolkit" ]; then
+    git clone https://github.com/VarunGumma/IndicTransToolkit.git
+else
+    echo "IndicTransToolkit already cloned."
+fi
+
+echo "Installing IndicTransToolkit in editable mode..."
+cd IndicTransToolkit
+python3 -m pip install --editable ./
+cd ..
+
+echo "IndicTransToolkit setup complete."
